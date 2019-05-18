4739b927b562a6af36a0978064625b8c738aae80
Serjio2888 <kerchtown@yandex.ru>
2019-05-18 21:58:54 +0300

    added changes

--- craw.py
+++ craw.py
@@ -14,53 +14,49 @@ class Parsing:
         self.urls = set()
         self.main = 'https://docs.python.org/'
         
+    async def crawling(self, q, session, es):
+        while True:
+            try:
+                link = await q.get()
+                print(link)
+                async with session.get(link) as r:                    
+                    html = await r.read()
+                    soup = BeautifulSoup(html, 'html.parser')
+                    doc = {
+                        'link': link,
+                        'text': soup.get_text(),
+                        'timestamp': datetime.now(),
+                        }                    
+                    await es.index(index="test-index", doc_type='tweet', body=doc)
 
-    async def crawling(self, session, es, link='https://docs.python.org/'):
-        try:
-            async with session.get(link) as r: 
-                
-                #r = requests.get(link)
-                html = await r.read()
-                soup = BeautifulSoup(html, 'html.parser')
-                
-                doc = {
-                    'link': link,
-                    'text': html,
-                    'timestamp': datetime.now(),
-                    }                    
-                await es.index(index="test-index", doc_type='tweet', body=doc)
-
-                findall = soup.find_all('link') + soup.find_all('a') 
-                for link in findall:
-                    link = link.get('href')
-                    
-                    if link not in self.urls:
-                        
-                        self.urls.add(link)
-                        if not link.startswith(self.main):
-                            if link.startswith('http'): #значит, это сайт другого домена
-                                continue
-                            if link.startswith('../'):
-                                link = self.main+link[3:]
-                            else:
-                                link = self.main+link
-
-                        await self.crawling(session, es, link)
-        except:
-            await es.close()
+                    findall = soup.find_all('link') + soup.find_all('a') 
+                    for link in findall:
+                        link = link.get('href')
+                        if link not in self.urls:
+                            self.urls.add(link)
+                            if not link.startswith(self.main):
+                                if link.startswith('http'): #значит, это сайт другого домена
+                                    continue
+                                if link.startswith('../'):
+                                    link = self.main+link[3:]
+                                    await q.put(link)
+                                else:
+                                    link = self.main+link
+                                    await q.put(link)
+            except:
+                await es.close()
     
     async def myfun(self, quantity):
-
         tasks = list()
+        q = asyncio.Queue()
+        q.put_nowait('https://docs.python.org/')
         async with aiohttp.ClientSession() as session:
+            es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
             for _ in range(quantity):
-                es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-                task = asyncio.create_task(self.crawling(session, es))
+                task = asyncio.create_task(self.crawling(q, session, es))
                 tasks.append(task)
             await asyncio.gather(*tasks)
 
-    #async def closing(self):
-    #    await self.es.close()
             
 
 p = Parsing()
